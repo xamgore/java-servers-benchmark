d9e5ae96b6fdc110840369a5505a859c24f89358
xamgore <xamgore@ya.ru>
2019-01-24 18:02:05 +0300

    Implement a server with common thread task executor (+tests)

--- c-server/src/main/java/server/CommonTaskExecutor.java
+++ c-server/src/main/java/server/CommonTaskExecutor.java
@@ -1,5 +1,6 @@
 package server;
 
+import com.google.protobuf.InvalidProtocolBufferException;
 import common.IntArrayOuterClass.IntArray;
 import common.SortingTask;
 
@@ -13,13 +14,20 @@ import java.net.SocketTimeoutException;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
 
-public class OneThreadPerClient implements Runnable {
+import static common.IntArrayOuterClass.IntArray.parseFrom;
+import static java.util.concurrent.Executors.newFixedThreadPool;
+import static java.util.concurrent.Executors.newSingleThreadExecutor;
+
+public class CommonTaskExecutor implements Runnable {
 
   private final int port;
   private final Set<ClientHolder> activeClients;
+  private final Executor executor = newFixedThreadPool(4);
 
-  public OneThreadPerClient(int port) {
+  public CommonTaskExecutor(int port) {
     this.port = port;
     this.activeClients = Collections.synchronizedSet(new HashSet<>());
   }
@@ -48,28 +56,26 @@ public class OneThreadPerClient implements Runnable {
   private class ClientHolder implements Runnable {
 
     final Socket socket;
+    private final DataInputStream in;
+    private final DataOutputStream out;
     Thread runningThread;
+    ExecutorService sendResponseExecutor;
 
-    public ClientHolder(Socket socket) {
+    public ClientHolder(Socket socket) throws IOException {
       this.socket = socket;
+      this.in = new DataInputStream(socket.getInputStream());
+      this.out = new DataOutputStream(socket.getOutputStream());
       this.runningThread = new Thread(this);
+      this.sendResponseExecutor = newSingleThreadExecutor();
     }
 
     @Override public void run() {
-      try (
-          DataInputStream in = new DataInputStream(socket.getInputStream());
-          DataOutputStream out = new DataOutputStream(socket.getOutputStream());
-      ) {
+      try {
         // client makes a finite number of requests
         while (!Thread.interrupted()) {
           byte[] buffer = new byte[in.readInt()];
           in.readFully(buffer);
-
-          IntArray task = IntArray.parseFrom(buffer);
-          IntArray result = SortingTask.complete(task);
-
-          out.writeInt(result.getSerializedSize());
-          result.writeTo(out);
+          executor.execute(solveTask(buffer));
         }
       } catch (EOFException ignored) {
       } catch (IOException e) {
@@ -79,6 +85,7 @@ public class OneThreadPerClient implements Runnable {
         activeClients.remove(this);
 
         try {
+          this.sendResponseExecutor.shutdown();
           socket.close();
         } catch (IOException e) {
           e.printStackTrace();
@@ -86,7 +93,31 @@ public class OneThreadPerClient implements Runnable {
       }
     }
 
-  }
+    private Runnable solveTask(byte[] buffer) {
+      return () -> {
+        try {
+          IntArray task = parseFrom(buffer);
+          IntArray result = SortingTask.complete(task);
+          sendResponseExecutor.execute(sendResponse(result));
+        } catch (InvalidProtocolBufferException e) {
+          e.printStackTrace();
+        }
 
+      };
+    }
+
+    private Runnable sendResponse(IntArray result) {
+      return () -> {
+        try {
+          out.writeInt(result.getSerializedSize());
+          result.writeTo(out);
+          out.flush();
+        } catch (IOException e) {
+          e.printStackTrace();
+        }
+      };
+    }
+
+  }
 
 }
