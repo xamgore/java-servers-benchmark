ff01f8b50af732c2dad8f34372b741eaa67d5caf
xamgore <xamgore@ya.ru>
2019-01-27 19:58:31 +0300

    Implement the server-side HTTP API

---
+++ c-server/src/main/java/server/Main.java
@@ -0,0 +1,80 @@
+package server;
+
+import fi.iki.elonen.NanoHTTPD;
+
+import java.io.IOException;
+import java.util.Map;
+
+import static java.lang.Integer.parseUnsignedInt;
+
+public class Main extends NanoHTTPD {
+
+  public static final int SERVER_PORT = 5400;
+
+  private Thread serverInstance;
+
+  private Main(int port) {
+    super(port);
+  }
+
+  public static void main(String[] args) {
+    try {
+      Main app = new Main(SERVER_PORT);
+      app.start(NanoHTTPD.SOCKET_READ_TIMEOUT, false);
+      System.out.printf("\nRunning! Point your browsers to http://localhost:%d/\n\n", app.getListeningPort());
+    } catch (IOException err) {
+      System.err.println("Couldn't start server:\n");
+      err.printStackTrace(System.err);
+    }
+  }
+
+  @Override
+  public Response serve(IHTTPSession session) {
+    String url = session.getUri();
+    Map<String, String> params = session.getParms();
+
+    if (url.equals("/start")) {
+      int archIdx = parseUnsignedInt(params.get("arch"));
+      startServer(archIdx);
+    } else if (url.equals("/stop")) {
+      stopServer();
+    } else if (!url.equals("/favicon.ico")) {
+      System.out.println(session.getUri());
+    }
+
+//    String msg = "<html><body><h1>Hello server</h1>\n";
+//    Map<String, String> parms = session.getParms();
+//    if (parms.get("username") == null) {
+//      msg += "<form action='?' method='get'>\n  <p>Your name: <input type='text' name='username'></p>\n" + "</form>\n";
+//    } else {
+//      msg += "<p>Hello, " + parms.get("username") + "!</p>";
+//    }
+    return newFixedLengthResponse("");
+  }
+
+  private void startServer(int architectureIdx) {
+    System.out.printf("/start arch=%d\n", architectureIdx);
+    stopServer();
+
+    Runnable architecture = architectureIdx == 0
+        ? new OneThreadPerClient(8080)
+        : new CommonTaskExecutor(8080);
+
+    serverInstance = new Thread(architecture);
+    serverInstance.start();
+    System.out.printf("%s was started\n", architecture.getClass().getSimpleName());
+  }
+
+  private void stopServer() {
+    System.out.println("/stop the current server");
+
+    try {
+      if (serverInstance == null) return;
+      serverInstance.interrupt();
+      serverInstance.join();
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+    }
+  }
+
+}
