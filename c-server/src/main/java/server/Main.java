4e814712fd02ee2c06f4c65e358106348929c7b7
xamgore <xamgore@ya.ru>
2019-01-29 14:37:43 +0300

    Refactor code: remove redundant commentary

--- c-server/src/main/java/server/Main.java
+++ c-server/src/main/java/server/Main.java
@@ -45,13 +45,6 @@ public class Main extends NanoHTTPD {
       System.out.println(session.getUri());
     }
 
-//    String msg = "<html><body><h1>Hello server</h1>\n";
-//    Map<String, String> parms = session.getParms();
-//    if (parms.get("username") == null) {
-//      msg += "<form action='?' method='get'>\n  <p>Your name: <input type='text' name='username'></p>\n" + "</form>\n";
-//    } else {
-//      msg += "<p>Hello, " + parms.get("username") + "!</p>";
-//    }
     return newFixedLengthResponse("");
   }
 
