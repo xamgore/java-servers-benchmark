0a139f42b31ad62d96be9574ff1e47b82c8fbdbf
xamgore <xamgore@ya.ru>
2019-02-04 05:50:50 +0300

    Add filters to the duration class: to measure the peak loading

--- c-common/src/main/java/common/Duration.java
+++ c-common/src/main/java/common/Duration.java
@@ -1,5 +1,6 @@
 package common;
 
+import java.util.Iterator;
 import java.util.Queue;
 import java.util.concurrent.ConcurrentLinkedQueue;
 
@@ -38,7 +39,21 @@ public class Duration {
       return System.currentTimeMillis();
     }
 
+
+    public long getRequestDuration() {
+      return requestEnd - requestStart;
+    }
+
+    public long getSortingDuration() {
+      return sortingEnd - sortingStart;
+    }
+
+    @Override public String toString() {
+      return String.format("Timer{request=%d, sorting=%d}", getRequestDuration(), getSortingDuration());
+    }
+
   }
+
 
   public static class Client {
 
@@ -50,14 +65,62 @@ public class Duration {
       return timer;
     }
 
+    public long connectionStart() {
+      return list.stream().mapToLong(t -> t.requestStart).min().orElse(Long.MAX_VALUE);
+    }
+
+    public long connectionEnd() {
+      return list.stream().mapToLong(t -> t.requestEnd).max().orElse(Long.MIN_VALUE);
+    }
+
+    public Client remainInRange(long start, long end) {
+      Iterator<Timer> iterator = list.iterator();
+
+      while (iterator.hasNext()) {
+        Timer timer = iterator.next();
+
+        if (timer.requestStart >= start && timer.requestEnd <= end)
+          continue;
+
+        iterator.remove();
+      }
+
+      return this;
+    }
+
   }
 
+
   public final Queue<Client> clients = new ConcurrentLinkedQueue<>();
+  public long lastConnect, firstDisconnect;
 
   public Client newClient() {
     Client instance = new Client();
     clients.add(instance);
     return instance;
+  }
+
+  public Duration remainOnlyHotDurations() {
+    lastConnect = clients.stream().mapToLong(Client::connectionStart).max().orElse(Long.MAX_VALUE);
+    firstDisconnect = clients.stream().mapToLong(Client::connectionEnd).min().orElse(Long.MIN_VALUE);
+    clients.forEach(client -> client.remainInRange(lastConnect, firstDisconnect));
+    return this;
+  }
+
+  public double avgRequestDuration() {
+    return clients.stream()
+        .flatMap(client -> client.list.stream())
+        .mapToLong(Timer::getRequestDuration)
+        .average()
+        .orElse(0);
+  }
+
+  public double avgSortingDuration() {
+    return clients.stream()
+        .flatMap(client -> client.list.stream())
+        .mapToLong(Timer::getSortingDuration)
+        .average()
+        .orElse(0);
   }
 
 }
